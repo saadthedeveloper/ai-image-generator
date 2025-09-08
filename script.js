d03acd49293fc190b4012490f25a2d8f272e35cb
saadthedeveloper <m.saad23145@gmail.com>
2025-09-08 17:15:34 +0500

    added theme logic

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 let themeButton = document.querySelector("#theme-button");
 let body = document.querySelector("body");
+let mainContainer = document.querySelector("#main-container");
 
 themeButton.addEventListener('click', () => {
-    if (themeButton.classList.contains("light-theme")) {
-        //Theme Button
-        themeButton.classList.remove("light-theme");
-        themeButton.classList.add("dark-theme")
+    if (themeButton.classList.contains("light-theme-button")) {
+        //Theme Button - will turn dark
+        themeButton.classList.remove("light-theme-button");
+        themeButton.classList.add("dark-theme-button");
+        themeButton.classList.remove("fa-moon");
+        themeButton.classList.add("fa-sun");
+
+        //Body - will turn dark
+        body.classList.remove("light-theme-body");
+        body.classList.add("dark-theme-body");
+
+        //Main Container - will turn dark
+        mainContainer.classList.add("dark-theme-main-container");
+        mainContainer.classList.remove("light-theme-body-main-container");
     } else {
-        //Theme Button
-        icon.classList.remove("dark-theme");
-        icon.classList.add("light-theme");
+        //Theme Button - will turn light
+        themeButton.classList.remove("dark-theme-button");
+        themeButton.classList.add("light-theme-button");
+        themeButton.classList.remove("fa-sun");
+        themeButton.classList.add("fa-moon");
+        
+        //Body - will turn light
+        body.classList.remove("dark-theme-body");
+        body.classList.add("light-theme-body");
+
+        //Main Container - will turn light
+        mainContainer.classList.remove("dark-theme-main-container");
+        mainContainer.classList.add("light-theme-main-container");
     }
 })
